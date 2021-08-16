fb400869e466fdc65930508cfff004ce7c3ea0a1
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2021-08-16 19:07:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const buttonElement = document.getElementById('btn');
+
+// Add a handler for the 'click' event by providing a callback function.
+// Whenever the element is clicked, a pop-up with "Element clicked!" will
+// appear.
+buttonElement.addEventListener('click', function(event) {
+    alert('Element clicked through function!');
+});
+
+// For compatibility, a non-function object with a `handleEvent` property is
+// treated just the same as a function itself.
+buttonElement.addEventListener('click', {
+    handleEvent: function(event) {
+        alert('Element clicked through handleEvent property!');
+    }
+});
+
+
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('event-type', function(event) {
+    // Handle event
+    ...
+})
+
+
+{
+    "richContent": [
+        [{
+                "type": "type-id",
+                ...
+            },
+            {
+                "type": "type-id",
+                ...
+            }
+        ],
+        [{
+                "type": "type-id",
+                ...
+            },
+            {
+                "type": "type-id",
+                ...
+            }
+        ]
+    ]
+}
